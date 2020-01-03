f5b2e2de26ab67a83a44f8ee30ec5184812207ad
dianafo98 <34780616+dianafo98@users.noreply.github.com>
2020-01-03 20:57:28 +0000

    Initial commit

---
+++ TP_PWEBfinal/TP_WEBtema/TP_WEBtema/TP_PWEB/ViewModels/EstacoesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP_PWEB.Models;
+
+namespace TP_PWEB.ViewModels
+{
+    public class EstacoesViewModel
+    {
+        public Estacao estacao { get; set; }
+        public bool error { get; set; }
+    }
+}
---
+++ TP_PWEBfinal/TP_WEBtema/TP_WEBtema/TP_PWEB/ViewModels/ReservaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP_PWEB.Models;
+
+namespace TP_PWEB.ViewModels
+{
+    public class ReservaViewModel
+    {
+        public List<Utilizador> Utilizadores { get; set; }
+        public List<Estacao> Estacoes { get; set; }
+        public int UtilizadorID { get; set; }
+
+    }
+}
