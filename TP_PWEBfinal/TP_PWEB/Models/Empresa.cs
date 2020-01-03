d7da371e013c3ec6b1ae66a42c91530af92c3767
dianafo98 <34780616+dianafo98@users.noreply.github.com>
2020-01-03 23:26:03 +0000

    modelo empresa

---
+++ TP_PWEBfinal/TP_PWEB/Models/Empresa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TP_PWEB.Models
+{
+    public class Empresa
+    {
+        public int EmpresaID { get; set; }
+        [Required]
+        [Display(Name = "Nome Empresa")]
+        public string NomeEmpresa { get; set;}
+
+        [Required]
+        [Display(Name = "Endereço Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string PerfilID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "A {0} tem de ter pelo menos {2} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Palavra-Passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Repita a Palavra-Passe")]
+        [Compare("Password", ErrorMessage = "As Palavra-Passe não coincidem.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
